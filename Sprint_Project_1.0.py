158c4b23985df743b4336563221bf8eabc06bed5
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-10 05:19:46 +0000

    Created and Updated python script

---
+++ Sprint_Project_1.0.py
@@ -0,0 +1,20 @@
+print ()
+print (4 + 6)
+print (3.5 + 2)
+# Example comment
+print(10 - 3)   # Integer subtraction
+print(10.5 - 3.5)  # Float subtraction
+print(8 * 3)
+print(7.5 * 2) # Multiplication of a Float and an Integer
+print(9 / 3) # Division of Integers
+print(7.8 / 2.5) # Division of Floats
+print(float(5)) # Integer to Float Conversion
+print(int(12.9)) # Float to Integer Conversion
+
+# Combining Operations (Addition and Conversion)
+result = 10 + 5.8
+print(int(result))
+
+# Use Floor Division to divide two integers and print the result.
+print(17 // 4)  # The result is 4
+
