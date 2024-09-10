ff4661d45ffd3b06352bb1a7a03695900e0cd8f6
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-10 06:17:11 +0000

    Created python script

---
+++ Sprint_Project_1.0.py
@@ -0,0 +1,25 @@
+print () # Comments can come AFTER code as well
+# Test comment 
+print (3 + 5)
+print (5.4 + 5)
+# This is a comment
+print (10 - 6)
+print (10.2 - 8.6)
+
+# This is how you name and call a function
+result = 10.2 - 6
+print (int(result))
+
+# Please print the following:
+# Multiply two intergers
+# Divide on floating point number by one interger
+# Convert one iterger to one floating point number
+# Convert one floating point number to one interger
+# Name a function "Sprint1" (without the quotes)
+# Set "Sprint1" equal to 
+# the operation where you 
+# Divide one interger by one floating point number
+# Call Sprint1 as an arguement in a print function
+# 
+Sprint1 = 7 / 2.5
+print (Sprint1)
